29758fdc6ca2a613697ee315d38d13139643a95e
Thomas Andrillon <thomas.andrillon@gmail.com>
2020-05-13 09:38:12 +1000

    adding tables for behaviour and local sleep

---
+++ Matlab_functions/Behaviour/CTET_analyse_behaviour_v2.m
@@ -0,0 +1,125 @@
+%%
+clear all
+close all
+
+path_fieldtrip='/Users/tand0009/Work/local/fieldtrip/';
+addpath(path_fieldtrip);
+ft_defaults;
+
+path_LSCPtools='/Users/tand0009/WorkGit/LSCPtools/';
+addpath(genpath(path_LSCPtools));
+
+data_path='/Volumes/tLab_BackUp1/Monash/CTET_Dockree/EEG_CTET/';
+% data_path='/Volumes/shared/R-MNHS-SPP/Bellgrove-data/Jess Barnes EEG Backup Data/EEG_CTET/';
+files=dir([data_path filesep '*.bdf']);
+
+%%
+res_mat=[];
+drug_cond=[];
+for nF=1:length(files)
+    File_Name=files(nF).name;
+    fprintf('... processing %s\n',File_Name);
+    septag=findstr(File_Name,'_');
+    SubN=str2num(File_Name(1:septag(1)-1));
+    SessN=str2num(File_Name(septag(3)-1));
+    DrugC=(File_Name(septag(3)+1:septag(3)+3));
+    
+    % data=ft_read_data('/Volumes/tLab_BackUp1/Monash/CTET_Dockree/EEG_CTET/01_ctet_session1_ATM.bdf');
+    hdr=ft_read_header([data_path filesep File_Name]);
+    events=ft_read_event([data_path filesep File_Name]);
+
+    my_events=events(find_trials({events.type},'STATUS'));
+    unique_values=unique([my_events.value]);
+    fprintf('... %g events found:',length([my_events.value]));
+    for nU=1:length(unique_values)
+        fprintf(' %g (n=%g) ',unique_values(nU),sum([my_events.value]==unique_values(nU)))
+    end
+    fprintf('\n');
+
+
+    %%% find blocks
+    all_idx=[my_events.sample];
+    all_val=[my_events.value];
+    blocks_boundaries=all_idx(find(diff(all_idx/hdr.Fs)>5 & all_val(2:end)==1)+1);
+    if all_val(1)==1
+        blocks_boundaries=[all_idx(1) blocks_boundaries];
+    end
+    fprintf('... %g block transitions found\n',length(blocks_boundaries));
+    all_val(ismember(all_idx,blocks_boundaries))=100;
+    
+    %%% find relevant events
+    resp_idx=[my_events(all_val==1).sample];
+    
+    targets_idx=[my_events(all_val==max(unique_values)).sample];
+    nontargets_idx=[my_events(all_val==20).sample];
+    stim_idx=[my_events(all_val==max(unique_values) | all_val==20).sample];
+    stim_val=[my_events(all_val==max(unique_values) | all_val==20).value];
+    
+    %%% Gather info per targets
+    behav_RT=nan(length(targets_idx),1);
+    behav_Miss=nan(length(targets_idx),1);
+    behav_Impulse=nan(length(targets_idx),1);
+    this_FAs=[];
+    for nTa=1:length(targets_idx)
+        this_target_idx=targets_idx(nTa);
+        count=0; prev_nontarget_idx=NaN;
+        while stim_val(find(ismember(stim_idx,targets_idx(nTa)))-count)~=20 && find(ismember(stim_idx,targets_idx(nTa)))-count>1
+            count=count+1;
+        end
+        if stim_val(find(ismember(stim_idx,targets_idx(nTa)))-count)==20
+            prev_nontarget_idx=stim_idx(find(ismember(stim_idx,targets_idx(nTa)))-count);
+        end
+        
+        count=0; next_nontarget_idx=NaN;
+        while stim_val(find(ismember(stim_idx,targets_idx(nTa)))+count)~=20 && (find(ismember(stim_idx,targets_idx(nTa)))+count)<length(stim_val)
+            count=count+1;
+        end
+        if stim_val(find(ismember(stim_idx,targets_idx(nTa)))+count)==20
+            next_nontarget_idx=stim_idx(find(ismember(stim_idx,targets_idx(nTa)))+count);
+        end
+        
+        count=0; next_resp_idx=NaN;
+        while all_val(find(ismember(all_idx,targets_idx(nTa)))+count)~=1 && (find(ismember(all_idx,targets_idx(nTa)))+count)<length(all_val)
+            count=count+1;
+        end
+        if all_val(find(ismember(all_idx,targets_idx(nTa)))+count)==1
+            next_resp_idx=all_idx(find(ismember(all_idx,targets_idx(nTa)))+count);
+        end
+        
+        %%% RT and lapses
+        this_RT=(next_resp_idx-this_target_idx)/hdr.Fs;
+        behav_RT(nTa)=this_RT;
+        if this_RT<2.6
+            if this_RT<mode(diff(nontargets_idx)/hdr.Fs)
+                behav_Impulse(nTa)=1;
+            else
+                behav_Impulse(nTa)=0;
+            end
+            behav_Miss(nTa)=0;
+        elseif this_RT>=2.6 && ~isnan(this_RT)
+            behav_Miss(nTa)=1;
+            behav_RT(nTa)=NaN;
+            behav_Impulse(nTa)=0;
+        end
+    end
+    
+    %%% Look for FAs
+    for nR=1:length(resp_idx)
+        dist_to_resp=(all_idx(find(ismember(all_idx,resp_idx(nR)))+(-10:0))-all_idx(find(ismember(all_idx,resp_idx(nR)))))/hdr.Fs;
+        if isempty(find(all_val(find(ismember(all_idx,resp_idx(nR)))+(-10:0))==max(unique_values) & dist_to_resp>-5.2))
+            this_FAs=[this_FAs ; resp_idx(nR)];
+        end
+    end
+    
+    res_mat=[res_mat; [SubN*ones(size(behav_Miss,1),1) SessN*ones(size(behav_Miss,1),1) behav_Miss behav_RT behav_Impulse (1:size(behav_Miss,1))']];
+    drug_cond=[drug_cond ; repmat({DrugC},size(behav_Miss,1),1)];
+end
+
+%%
+res_mat2=res_mat(~isnan(res_mat(:,3)),:);
+table=array2table(res_mat2,'VariableNames',{'SubID','SessN','Miss','RT','Impulse','TrialN'});
+table.Treatment=drug_cond(~isnan(res_mat(:,3)));
+% table.Treatment(match_str(table.Treatment,'MPH_run-at-wrong_Hz-rate'))={'MPH'};
+% table.Treatment(match_str(table.Treatment,'ATM_run-at-wrong-Hz-rate'))={'ATM'};
+table.Treatment=categorical(table.Treatment);
+table.Treatment=reordercats(table.Treatment,[4,1,2,3]);
---
+++ Matlab_functions/Behaviour/CTET_analyse_behaviour_v3.m
@@ -0,0 +1,189 @@
+%%
+clear all
+close all
+
+path_fieldtrip='/Users/tand0009/Work/local/fieldtrip/';
+addpath(path_fieldtrip);
+ft_defaults;
+
+path_LSCPtools='/Users/tand0009/WorkGit/LSCPtools/';
+addpath(genpath(path_LSCPtools));
+
+data_path='/Volumes/tLab_BackUp1/Monash/CTET_Dockree/EEG_CTET/';
+% data_path='/Volumes/shared/R-MNHS-SPP/Bellgrove-data/Jess Barnes EEG Backup Data/EEG_CTET/';
+files=dir([data_path filesep '*.bdf']);
+
+%%
+res_mat=[];
+drug_cond=[];
+for nF=1:length(files)
+    File_Name=files(nF).name;
+    fprintf('... processing %s\n',File_Name);
+    septag=findstr(File_Name,'_');
+    SubN=str2num(File_Name(1:septag(1)-1));
+    SessN=str2num(File_Name(septag(3)-1));
+    DrugC=(File_Name(septag(3)+1:septag(3)+3));
+    
+    % data=ft_read_data('/Volumes/tLab_BackUp1/Monash/CTET_Dockree/EEG_CTET/01_ctet_session1_ATM.bdf');
+    hdr=ft_read_header([data_path filesep File_Name]);
+    events=ft_read_event([data_path filesep File_Name]);
+
+    my_events=events(find_trials({events.type},'STATUS'));
+    unique_values=unique([my_events.value]);
+    fprintf('... %g events found:',length([my_events.value]));
+    for nU=1:length(unique_values)
+        fprintf(' %g (n=%g) ',unique_values(nU),sum([my_events.value]==unique_values(nU)))
+    end
+    fprintf('\n');
+
+
+    %%% find blocks
+    all_idx=[my_events.sample];
+    all_val=[my_events.value];
+    blocks_boundaries=all_idx(find(diff(all_idx/hdr.Fs)>5 & all_val(2:end)==1)+1);
+    if all_val(1)==1
+        blocks_boundaries=[all_idx(1) blocks_boundaries];
+    end
+    fprintf('... %g block transitions found\n',length(blocks_boundaries));
+    all_val(ismember(all_idx,blocks_boundaries))=100;
+    
+    %%% find relevant events
+    resp_idx=[my_events(all_val==1).sample];
+    
+    targets_idx=[my_events(all_val==max(unique_values)).sample];
+    nontargets_idx=[my_events(all_val==20).sample];
+    stim_idx=[my_events(all_val==max(unique_values) | all_val==20).sample];
+    stim_val=[my_events(all_val==max(unique_values) | all_val==20).value];
+    
+    %%% Gather info per targets
+    this_behav=nan(length(stim_idx),7);
+    for nSt=1:length(all_idx)
+        this_FA=NaN;
+        this_RT=NaN;
+        if all_val(nSt)==max(unique_values) % we have a target
+            count=0; resprec=0;
+            while nSt+count<length(all_val) && all_val(nSt+count)~=1
+                count=count+1;
+            end
+            if all_val(nSt+count)==1
+                this_RT=(all_idx(nSt+count)-all_idx(nSt))/hdr.Fs;
+            else
+                this_RT=NaN;
+            end
+        elseif all_val(nSt)==20 % we have a non-target
+            if nSt<length(all_val) & nSt>5
+                if all_val(nSt+1)==1 && max(all_val(nSt+(-5:-1)))~=max(unique_values)
+                    this_FA=1;
+                else
+                    this_FA=0;
+                end
+            end
+        else
+            continue;
+        end
+        %%%% sort things out
+        this_block=max(find(all_idx(nSt)>blocks_boundaries));
+        if isempty(this_block)
+            this_block=NaN;
+        end
+        if ~isnan(this_RT)
+            if this_RT>2.6
+                this_Miss=1;
+            else
+                this_Miss=0;
+            end
+        elseif isnan(this_RT) && all_val(nSt)==max(unique_values) && nSt~=length(all_idx)
+            this_Miss=1;
+        else
+            this_Miss=NaN;
+        end
+        this_behav(find(stim_idx==all_idx(nSt)),:)=[find(stim_idx==all_idx(nSt)) stim_idx(find(stim_idx==all_idx(nSt))) this_block all_val(nSt)==max(unique_values) this_RT this_FA this_Miss];
+    end
+    this_behav(this_behav(:,5)<mode(diff(nontargets_idx)/hdr.Fs),7)=1;
+    this_behav(find(this_behav(:,5)<mode(diff(nontargets_idx)/hdr.Fs))-1,6)=1;
+    this_cleanRT=this_behav(:,5);
+    this_cleanRT(this_cleanRT>2.6 | this_cleanRT<mode(diff(nontargets_idx)/hdr.Fs))=NaN;
+    res_mat=[res_mat; [SubN*ones(size(this_behav,1),1) SessN*ones(size(this_behav,1),1) this_behav this_cleanRT]];
+    drug_cond=[drug_cond ; repmat({DrugC},size(this_behav,1),1)];
+end
+
+%%
+table=array2table(res_mat,'VariableNames',{'SubID','SessN','TrialN','Sample','BlockN','StimType','All_RT','FA','Miss','Hit_RT'});
+table.Treatment=drug_cond(~isnan(res_mat(:,3)));
+% table.Treatment(match_str(table.Treatment,'MPH_run-at-wrong_Hz-rate'))={'MPH'};
+% table.Treatment(match_str(table.Treatment,'ATM_run-at-wrong-Hz-rate'))={'ATM'};
+table.SubID=categorical(table.SubID);
+table.SessN=categorical(table.SessN);
+table.Treatment=categorical(table.Treatment);
+table.Treatment=reordercats(table.Treatment,[4,1,2,3]);
+
+table.TrialN=ordinal(table.TrialN);
+table.BlockN=ordinal(table.BlockN);
+
+mdl1=fitlme(table,'Hit_RT~1+Treatment+(1|SubID)');
+mdl2=fitlme(table,'FA~1+Treatment+(1|SubID)');
+mdl3=fitlme(table,'Miss~1+Treatment+(1|SubID)');
+
+writetable(table,'/Users/tand0009/Data/CTET_Dockree/CTET_behav_res.txt');
+%%
+uniqueIDs=unique(table.SubID);
+uniqueDrugs=unique(table.Treatment);
+labelsDrugs=[];
+for nSubj=1:length(uniqueIDs)
+    for nDrug=1:length(uniqueDrugs)
+        Hit_RT(nSubj,nDrug)=nanmean(table.Hit_RT(table.SubID==uniqueIDs(nSubj) & table.Treatment==uniqueDrugs(nDrug)));
+        FA(nSubj,nDrug)=nanmean(table.FA(table.SubID==uniqueIDs(nSubj) & table.Treatment==uniqueDrugs(nDrug)));
+        Miss(nSubj,nDrug)=nanmean(table.Miss(table.SubID==uniqueIDs(nSubj) & table.Treatment==uniqueDrugs(nDrug)));
+        labelsDrugs{nDrug}=char(uniqueDrugs(nDrug));
+    end
+end
+
+Colors=[[1 1 1]*0.5; [253,174,97]/256 ; [44,123,182]/256; [215,25,28]/256];
+figure; set(gcf,'Position',[82         600        1158         378]);
+for nPlot=1:3
+    subplot(1,3,nPlot); format_fig; hold on;
+    if nPlot==1
+        temp=Hit_RT;
+    elseif nPlot==2
+        temp=FA;
+    else
+        temp=Miss;
+    end
+    format_fig;
+    for nDrug=1:length(uniqueDrugs)
+        simpleBarPlot(nDrug,temp(:,nDrug),Colors(nDrug,:),0.8,'k',[],3);
+    end
+    set(gca,'XTick',1:length(uniqueDrugs),'XTickLabel',labelsDrugs);
+    if nPlot==1
+    ylim([1.2 1.5])
+        title('Hit RT')
+    elseif nPlot==2
+        title('FA')
+    else
+        title('Miss')
+    end
+end
+
+%%
+load('/Users/tand0009/Data/CTET_Dockree/VAS_alertness_exclusions.mat');
+figure; %set(gcf,'Position',[82         600        1158         378]);
+for nTime=1:3
+    subplot(2,1,1); format_fig
+    for nDrug=1:4
+        eval(sprintf('temp=VASalertnessexclusions.%s_VS%g;',labelsDrugs{nDrug},nTime));
+        simpleBarPlot(nDrug+(nTime-2)*0.2,temp,Colors(nDrug,:),0.15,'k',[],3);
+    end
+        set(gca,'XTick',1:length(uniqueDrugs),'XTickLabel',labelsDrugs);
+
+        subplot(2,1,2); format_fig
+        eval(sprintf('temp0=VASalertnessexclusions.%s_VS%g;',labelsDrugs{1},nTime));
+    for nDrug=2:4
+        eval(sprintf('temp=VASalertnessexclusions.%s_VS%g;',labelsDrugs{nDrug},nTime));
+        simpleBarPlot(nDrug+(nTime-2)*0.2,temp./temp0,Colors(nDrug,:),0.15,'k',[],3);
+    end
+        set(gca,'XTick',2:length(uniqueDrugs),'XTickLabel',labelsDrugs(2:4));
+line(xlim,[1 1],'LineStyle','--','Color','k');
+end
+
+
+
