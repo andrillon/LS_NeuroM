0b4897cc51cc0cd72b8d00138a1cf485d52b758f
Thomas Andrillon <thomas.andrillon@gmail.com>
2020-06-10 11:22:30 +1000

    v4 Behav function and simpleTopoPlot with tvalues

--- Matlab_functions/LocalSleepDetection/CTET_analyse_LS_simplePlot_v1.m
+++ Matlab_functions/LocalSleepDetection/CTET_analyse_LS_simplePlot_v1.m
@@ -15,7 +15,7 @@ cfg = [];
 cfg.layout = 'biosemi64.lay';
 layout=ft_prepare_layout(cfg);
 
-cmap=cbrewer('seq','YlOrRd',64);
+cmap=cbrewer('seq','YlOrRd',64); % select a sequential colorscale from yellow to red (64)
 Drugs={'PLA','ATM','CIT','MPH'};
 
 limMax=13;
@@ -23,7 +23,7 @@ limMax=13;
 figure;
 temp_topo=[];
 for nE=1:64
-    temp_topo(nE)=mean(table_SW.SWdens(~cellfun(@isempty,regexp(table_SW.Elec,layout.label{nE}))));
+    temp_topo(nE)=mean(table_SW.SWdens(find_trials(table_SW.Elec,layout.label{nE})));
 end
 simpleTopoPlot_ft(temp_topo', layout,'on',[],0,1);
 title('All'); 
@@ -33,6 +33,39 @@ colormap(cmap);
 ylabel(h, 'waves/min')
 %     set(h,'Position',[0.85 0.7 0.04 0.2])
 format_fig;
+
+%% By Block
+limMax=13;
+
+figure;
+for nBl=1:10
+    subplot(2,5,nBl)
+    sub_table_SW=table_SW(table_SW.BlockN==nBl,:);
+    temp_topo=[];
+    for nE=1:64
+        temp_topo(nE)=mean(sub_table_SW.SWdens(find_trials(sub_table_SW.Elec,layout.label{nE})));
+    end
+    simpleTopoPlot_ft(temp_topo', layout,'on',[],0,1);
+    title(sprintf('Block %g',nBl));
+    caxis([0 1]*limMax)
+%     h=colorbar;
+    colormap(cmap);
+%     ylabel(h, 'waves/min')
+    %     set(h,'Position',[0.85 0.7 0.04 0.2])
+    format_fig;
+end
+
+figure;
+temp_plot=[];
+for nBl=1:10
+    sub_table_SW=table_SW(table_SW.BlockN==nBl,:);
+    temp_plot(1,nBl)=mean(sub_table_SW.SWdens);
+    temp_plot(2,nBl)=sem(sub_table_SW.SWdens);
+end
+plot(1:10,temp_plot(1,:),'Color','k','Marker','o','LineWidth',3)
+hold on;
+errorbar(1:10,temp_plot(1,:),temp_plot(2,:),'Color','k','LineWidth',2)
+    format_fig;
 
 %% By Drug
 figure; hold on; set(gcf,'Position',[201         428        1135         500]);
@@ -108,3 +141,30 @@ mdl4=fitlme(table_SW2,'SWdens~1+Elec*(BlockN+Drug)+(1|SubID)');
 mdl5=fitlme(table_SW2,'SWdens~1+Elec*BlockN*Drug+(1|SubID)');
 
 compare(mdl4,mdl5)
+
+%%
+temp_topo_tval=[];
+for nE=1:64
+    sub_table_SW2=table_SW2(find_trials(sub_table_SW.Elec,layout.label{nE}),:);
+    mdl_byEle{nE}=fitlme(sub_table_SW2,'SWdens~1+Drug+(1|SubID)');
+    temp_topo_tval(nE,:)=double(mdl_byEle{nE}.Coefficients(2:4,4));
+end
+
+%%
+cmap2=cbrewer('div','RdBu',64); % select a sequential colorscale from yellow to red (64)
+cmap2=flipud(cmap2);
+
+limMax=8.2;
+figure;
+for nDrug=1:3
+    subplot(1,3,nDrug)
+    simpleTopoPlot_ft(temp_topo_tval(:,nDrug), layout,'on',[],0,1);
+    format_fig;
+    caxis([-1 1]*limMax)
+    if nDrug==3
+        h=colorbar;
+        set(h,'Position',[0.93 0.4 0.02 0.2])
+    end
+    colormap(cmap2);
+    title(Drugs{nDrug+1})
+end
